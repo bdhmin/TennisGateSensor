9e7f86b585363c9bf399ba98f3bf2d6dac7e871e
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2021-04-07 15:12:03 -0700

    Added blinktest files for receiver and transmitter

---
+++ Receiver/rx_blinktest.ino
@@ -0,0 +1,30 @@
+#define ledPin 2
+String incomingString;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()) {
+    incomingString = Serial.readString();
+
+    /*
+    if(incomingString.indexOf("LEDON") >0) {
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(incomingString.indexOf("LEDOFF") >0) {
+      digitalWrite(ledPin, LOW);
+    }
+    */
+
+    if (incomingString.indexOf("HI") >0) {
+        digitalWrite(ledPin, HIGH);
+        delay(100);
+        digitalWrite(ledPin, LOW);
+    }
+  }
+}
